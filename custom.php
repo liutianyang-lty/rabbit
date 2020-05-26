c20a6f5667c92b4ed1581c69f9f1c6ca495000e8
e <liuyangyang@shenkeinfo.com>
2020-05-26 11:03:57 +0800

    custom.php

--- custom.php
+++ custom.php
@@ -5,14 +5,14 @@
  * Date: 2020/5/25 0025
  * Time: 23:02
  */
-
+define('BASE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
 include __DIR__ . "/App/Lib/Rabbit.php";
-//use App\Lib\Rabbit;
+$arr =  include(__DIR__ . "/Config/rabbit.php");
+
 $fun = function ($envelope, $queue){
 
     //消费 （刚取出数据，业务终止了；取出消息，执行不成功）怎么保证消息的可靠性
     //这里是写业务逻辑的地方
-
     //ack 应答机制
     var_dump($envelope->getBody());
 };
