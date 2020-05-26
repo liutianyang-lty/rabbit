179f2e017f13b243af2609f2976e880d66ab16cc
e <liuyangyang@shenkeinfo.com>
2020-05-26 10:16:06 +0800

    test

--- produ.php
+++ produ.php
@@ -9,7 +9,7 @@ define('BASE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
 
 //echo BASE_PATH;
 include __DIR__ . "/App/Lib/Rabbit.php";
-
+$arr =  include(__DIR__ . "/Config/rabbit.php");
 //$instance = Rabbit::getInstance()->listen("jiaohuanji", "queue1");
 var_dump(Rabbit::getInstance());die;
 $instance->publish("请求已发送");
