12c980109e264f440c52f1ae8d871bb5e820eb16
e <liuyangyang@shenkeinfo.com>
2020-05-26 09:46:43 +0800

    定义项目跟路径

--- produ.php
+++ produ.php
@@ -5,6 +5,9 @@
  * Date: 2020/5/25 0025
  * Time: 22:59
  */
+define('BASE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
+
+echo BASE_PATH;
 include __DIR__ . "/App/Lib/Rabbit.php";
 //use App\Lib\Rabbit;
 
